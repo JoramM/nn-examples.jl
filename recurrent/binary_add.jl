1141c4819d6d431e7393d3c0745fc5482383dbd6
JoramM <jorammarkert@gmx.de>
2017-12-14 16:40:55 +0100

    Do some optimization and cleanup

--- recurrent/binary_add.jl
+++ recurrent/binary_add.jl
@@ -1,71 +1,6 @@
-using Flux
-using Flux: mse, throttle, crossentropy
-using Gallium
-import NNlib.σ
-# using Base.Iterators: repeated
-
-# input variables
-bin_length = 8
-max_num = 2^bin_length
-
-α = 0.1                     # learning rate
-input_dim = 2               # two input numbers
-hidden_dim = 7              # hidden neurons
-output_dim = 1              # the output is sum of the two numbers
-set_size = 15000               # size of the training set
-
-# int to binary dict
-int_to_bin = Dict(i => bin(i,bin_length) for i = 0:max_num)
-
-# training data
-num_x = reshape([convert(Int16,rand(0:max_num/2)) for i = 1:set_size*2], 2, set_size)
-num_y = reshape([num_x[1,i] + num_x[2,i] for i = 1:set_size], 1, set_size)
-
-# x, y = Array{Array{Float64,input_dim}}, Array{Array{Float64,output_dim}}
-# x, y = [], []
-
-# convert to bin
-
-# for i = 1:set_size
-#     a = bin(num_x[1,i],bin_length)
-#     b = bin(num_x[2,i],bin_length)
-#     c = bin(num_y[i],bin_length)
-#
-#     in_bin = zeros(2,bin_length)
-#     out_bin = zeros(1,bin_length)
-#     for j = 1:bin_length
-#         in_bin[1,j] = convert(Float64, a[j])-48
-#         in_bin[2,j] = convert(Float64, b[j])-48
-#         out_bin[j] = convert(Float64, c[j])-48
-#     end
-#     push!(x,in_bin)
-#     push!(y,out_bin)
-# end
-
-# model
-# m = Chain(
-#     RNN(input_dim, hidden_dim, σ),
-#     Dense(hidden_dim, output_dim, σ))
-
-function print_status(err, label, pred)
-    println("Error: ", err)
-    println("True: ", label)
-    println("Pred: ", pred)
-end
-
-# weight matrices
-syn_0 = 2*randn(input_dim,hidden_dim)
-syn_1 = 2*randn(hidden_dim,output_dim)
-syn_r = 2*randn(hidden_dim,hidden_dim)
-
-syn_0_update = zeros(size(syn_0))
-syn_1_update = zeros(size(syn_1))
-syn_r_update = zeros(size(syn_r))
-
-# loss function
-# loss(x,y) = mse(m(x),y)
+# define some functions
 loss(x,y) = x - y
-# σ(x) = 1/(1+exp.(-x))
+
 function σ(x)
     x = 1+exp.(-x)
     for i = 1:length(x)
@@ -92,18 +27,57 @@ function vecprod(a,b)
     end
 end
 
-# dataset = zip(x,y)
-# dataset = zip(xs, ys)
-# opt = SGD(params(m), α)             # Gradient descent with learning rate 0.25
-# evalcb = () -> @show(loss(x, y))    # output the progress
+function print_status(err, label, pred)
+    println("Error: ", err)
+    println("True: ", label)
+    println("Pred: ", pred)
+end
+
+# input variables
+bin_length = 8
+max_num = 2^bin_length
+
+α = 0.1                     # learning rate
+input_dim = 2               # two input numbers
+hidden_dim = 9              # hidden neurons
+output_dim = 1              # the output is sum of the two numbers
+set_size = 15000            # size of the training set
+
+# training data
+num_x = reshape([convert(Int16,rand(0:max_num/2)) for i = 1:set_size*2], 2, set_size)
+num_y = reshape([num_x[1,i] + num_x[2,i] for i = 1:set_size], 1, set_size)
+
+# convert to bin
+x, y = [], []
+for i = 1:set_size
+    a = bin(num_x[1,i],bin_length)
+    b = bin(num_x[2,i],bin_length)
+    c = bin(num_y[i],bin_length)
+
+    in_bin = zeros(2,bin_length)
+    out_bin = zeros(1,bin_length)
+    for j = 1:bin_length
+        in_bin[1,j] = convert(Float64, a[j])-48
+        in_bin[2,j] = convert(Float64, b[j])-48
+        out_bin[j] = convert(Float64, c[j])-48
+    end
+    push!(x,in_bin)
+    push!(y,out_bin)
+end
+
+# weight matrices
+syn_0 = 2*randn(input_dim,hidden_dim)
+syn_1 = 2*randn(hidden_dim,output_dim)
+syn_r = 2*randn(hidden_dim,hidden_dim)
+
+syn_0_update = zeros(size(syn_0))
+syn_1_update = zeros(size(syn_1))
+syn_r_update = zeros(size(syn_r))
 
 # training
 for i = 1:set_size
-    in_bin_a = bin(num_x[1,i],bin_length)   # addend 1 (input 1)
-    in_bin_b = bin(num_x[2,i],bin_length)   # addend 2 (input 2)
-    out_bin_c = bin(num_y[i],bin_length)    # currect sum (output)
 
-    prediction = zeros(bin_length)          # result of the net
+    prediction = reshape(zeros(bin_length),1,bin_length)          # result of the net
     overallError = 0
 
     layer_2_Δs = []
@@ -112,20 +86,14 @@ for i = 1:set_size
 
     # calculate output and error
     for pos = bin_length:-1:1
-        a = convert(Float64, in_bin_a[pos])-48
-        b = convert(Float64, in_bin_b[pos])-48
-
-        x = [a b]
-        y = [convert(Float64, out_bin_c[pos])-48]'
-
         # MODEL -->
         # hidden layer (input ~+ prev_hidden)
-        layer_1 = σ(x*syn_0 + layer_1_values[end]*syn_r)
+        layer_1 = σ(x[i][1:end, pos]'*syn_0 + layer_1_values[end]*syn_r)
 
         # output layer (new binary representation)
         layer_2 = σ(layer_1*syn_1)
 
-        layer_2_error = loss(y,layer_2)
+        layer_2_error = loss(y[i][pos],layer_2)
         push!(layer_2_Δs, layer_2_error*σ_derivative(layer_2))     # save derivatives for each timestep
         overallError += abs(layer_2_error[1])
 
@@ -139,7 +107,6 @@ for i = 1:set_size
 
     # backpropagation
     for pos = 1:bin_length
-        x = [convert(Float64, in_bin_a[pos])-48,convert(Float64, in_bin_b[pos])-48]
         layer_1 = layer_1_values[end-pos+1]        # selecting the current hidden layer
         prev_layer_1 = layer_1_values[end-pos]     # selecting the previous hidden layer
 
@@ -151,7 +118,7 @@ for i = 1:set_size
         # let's update all our weights so we can try again
         syn_1_update += layer_1'*layer_2_Δ
         syn_r_update += prev_layer_1'*layer_1_Δ
-        syn_0_update += x*layer_1_Δ
+        syn_0_update += x[i][1:end, pos]*layer_1_Δ
 
         future_layer_1_Δ = layer_1_Δ
     end
@@ -166,14 +133,6 @@ for i = 1:set_size
     syn_r_update *= 0
 
     if i % (0.1*set_size) == 0
-        print_status(overallError, out_bin_c, prediction)
+        print_status(overallError, y[i], prediction)
     end
 end
-
-# Flux.train!(loss, dataset, opt, cb = throttle(evalcb, 0.5))
-# Flux.train!(loss, dataset, opt)
-
-# output final error
-# @show overallError
-
-# testing the results
